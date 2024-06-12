a7e4641c33d93aed2adadcb6c1f9b13625e8d17d
TeJ VarshIth <116535984+Varshi45@users.noreply.github.com>
2024-06-12 14:50:19 +0530

    Update Techstack.js

--- src/pages/Techstack/Techstack.js
+++ src/pages/Techstack/Techstack.js
@@ -8,32 +8,38 @@ const Techstack = ({ sidebarToggle }) => {
   const containerClass = sidebarToggle ? "techstack" : "techstack-toggled";
   const [theme] = useTheme();
 
-  return (
-    <div id="techstack" className={containerClass}>
-      <h2 className="text-center">Tech-Stack</h2>
-      <hr />
-      <p className="text-center">
-        including programming languages, tools, frameworks, and databases.
-      </p>
+  const techstackGroupedByType = TechstackList.reduce((acc, tech) => {
+    if (!acc[tech.type]) acc[tech.type] = [];
+    acc[tech.type].push(tech);
+    return acc;
+  }, {});
 
-      <div className={`row row-cols-1 row-cols-md-3 row-cols-lg-4 g-4 techstack-list ${sidebarToggle ? '' : 'justify-content-center'}`}>
-        {TechstackList.map((tech) => (
-          <div
-            key={tech._id}
-            className="col"
-          >
+  const renderTechstackSection = (sectionTitle, techList) => (
+    <div className="techstack-section" key={sectionTitle}>
+      <h3 className="my-3">{sectionTitle}</h3>
+      <div className={`row row-cols-1 row-cols-md-3 row-cols-lg-4 g-4 techstack-list`}>
+        {techList.map((tech) => (
+          <div key={tech._id} className="col">
             <Card className="techstack-card text-center">
               <Card.Body>
-                <tech.icon
-                  className="techstack-icon"
-                  style={{ color: theme === "dark" ? "#ff9d00" : "#dc5f00" }}
-                />
+                <tech.icon className="techstack-icon" style={{ color: theme === "dark" ? "#ff9d00" : "#dc5f00" }} />
                 <Card.Title className="techstack-name">{tech.name}</Card.Title>
               </Card.Body>
             </Card>
           </div>
         ))}
       </div>
+    </div>
+  );
+
+  return (
+    <div id="techstack" className={containerClass}>
+      <h2 className="text-center">Tech-Stack</h2>
+      <hr />
+
+      {Object.keys(techstackGroupedByType).map((type) =>
+        renderTechstackSection(type, techstackGroupedByType[type])
+      )}
     </div>
   );
 };
